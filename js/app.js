3f45b0c069ea204d7f61d54863ceeaac48cb49ea
Mia <54636763+miamija7@users.noreply.github.com>
2023-01-12 12:38:37 -0600

    preview title

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const appleId = '1629208237';
+const loadPodcastByAppleId = async (id) => {
+    try {
+        const res = await fetch(`https://itunes.apple.com/lookup?id=${id}`);
+        const data = await res.json();
+        console.log(data);
+    } catch (e) {
+        console.log("ERROR:", e);
+    }
+}
+
