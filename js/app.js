9a2865c118542799b9a972093b373010914fb53a
Mia <54636763+miamija7@users.noreply.github.com>
2023-01-12 18:58:58 -0600

    the handlebar podcast mobile preview

--- js/app.js
+++ js/app.js
@@ -9,104 +9,104 @@ const loadPodcastByAppleId = async (id) => {
     }
 }
 
-////////  TRUNK START  ////////
-VANTA.TRUNK({
-    el: "#your-element-selector",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0x1327bb,
-    spacing: 7.0,
-    chaos: 5.00
-})
-////////  TRUNK END  ////////
-
-
-////////  MORPH START  ////////
-const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
-};
-
-const texts = [
-    "The",
-    "Handlebar",
-    "Podcast"
-];
-
-const morphTime = 1;
-const cooldownTime = 1.75;
-
-let textIndex = texts.length - 1;
-let time = new Date();
-let morph = 0;
-let cooldown = cooldownTime;
-
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-
-function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
-
-    let fraction = morph / morphTime;
-
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
-
-    setMorph(fraction);
-}
-
-function setMorph(fraction) {
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-}
-
-function doCooldown() {
-    morph = 0;
-
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
-
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
-
-    cooldown -= dt;
-
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
-
-        doMorph();
-    } else {
-        doCooldown();
-    }
-}
-animate();
-////////  MORPH START  ////////
+// ////////  TRUNK START  ////////
+// VANTA.TRUNK({
+//     el: "#your-element-selector",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0x1327bb,
+//     spacing: 7.0,
+//     chaos: 5.00
+// })
+// ////////  TRUNK END  ////////
+//
+//
+// ////////  MORPH START  ////////
+// const elts = {
+//     text1: document.getElementById("text1"),
+//     text2: document.getElementById("text2")
+// };
+//
+// const texts = [
+//     "The",
+//     "Handlebar",
+//     "Podcast"
+// ];
+//
+// const morphTime = 1;
+// const cooldownTime = 1.75;
+//
+// let textIndex = texts.length - 1;
+// let time = new Date();
+// let morph = 0;
+// let cooldown = cooldownTime;
+//
+// elts.text1.textContent = texts[textIndex % texts.length];
+// elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+//
+// function doMorph() {
+//     morph -= cooldown;
+//     cooldown = 0;
+//
+//     let fraction = morph / morphTime;
+//
+//     if (fraction > 1) {
+//         cooldown = cooldownTime;
+//         fraction = 1;
+//     }
+//
+//     setMorph(fraction);
+// }
+//
+// function setMorph(fraction) {
+//     elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+//
+//     fraction = 1 - fraction;
+//     elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+//     elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+//
+//     elts.text1.textContent = texts[textIndex % texts.length];
+//     elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+// }
+//
+// function doCooldown() {
+//     morph = 0;
+//
+//     elts.text2.style.filter = "";
+//     elts.text2.style.opacity = "100%";
+//
+//     elts.text1.style.filter = "";
+//     elts.text1.style.opacity = "0%";
+// }
+//
+// function animate() {
+//     requestAnimationFrame(animate);
+//
+//     let newTime = new Date();
+//     let shouldIncrementIndex = cooldown > 0;
+//     let dt = (newTime - time) / 1000;
+//     time = newTime;
+//
+//     cooldown -= dt;
+//
+//     if (cooldown <= 0) {
+//         if (shouldIncrementIndex) {
+//             textIndex++;
+//         }
+//
+//         doMorph();
+//     } else {
+//         doCooldown();
+//     }
+// }
+// animate();
+// ////////  MORPH START  ////////
 
 ////////  SNAP START  ////////
 const gra = function(min, max) {
