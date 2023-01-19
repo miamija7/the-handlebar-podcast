36a2fac48f1b7ac85d30fa553ab7c8dca32319aa
Mia <54636763+miamija7@users.noreply.github.com>
2023-01-19 16:04:17 -0600

    home page

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/////// LIVE CHAR COUNT START ///////
+var text_max = 120;
+$('#count_message').html('0 / ' + text_max );
+
+$('#Question').keyup(function() {
+    var text_length = $('#Question').val().length;
+    var text_remaining = text_max - text_length;
+
+    $('#count_message').html(text_length + ' / ' + text_max);
+});
+
+$('#button-next').click(function (){
+    $('.form-pt2').toggleClass('hidden');
+    $('.form-pt1').toggleClass('hidden');
+})
+
+$('#button-back').click(function (){
+    $('.form-pt2').toggleClass('hidden');
+    $('.form-pt1').toggleClass('hidden');
+})
+/////// LIVE CHAR COUNT END ///////
+
+
+/////// POST FORM START ///////
+window.addEventListener("load", function() {
+    const form = document.getElementById('ask-a-question');
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        }).then(() => {
+            form.reset();
+            $('.form-pt2').toggleClass('hidden');
+            $('.form-pt1').toggleClass('hidden');
+            alert("Your question was submitted! ❤️");
+        })
+    });
+});
+/////// POST FORM END ///////
